79a2822a07be2bce075f72e2db73bc0438e5cbbf
Johanne Khan Malek <johannekhanmalek@johannes-mbp.lan>
2023-11-06 16:54:59 +0100

    with some css

--- src/App.jsx
+++ src/App.jsx
@@ -23,16 +23,56 @@ const App = () => {
     <p>Loading...</p>
   ) : (
     <>
-      <header></header>
+      <header>
+        <img src="./src/assets/logo-teal.svg" alt="logo" />
+      </header>
+
       <section className="presentation">
-        <h1> {data.restaurant.name} </h1>
-        <p> {data.restaurant.description}</p>
-        <img src="" alt="" />
+        <div className="container">
+          <div className="text-presentation">
+            <h1> {data.restaurant.name} </h1>
+            <p> {data.restaurant.description}</p>
+          </div>
+          <img src={data.restaurant.picture} alt="header-img" />
+        </div>
       </section>
 
       <section className="menu">
         {data.categories.map((elem) => {
-          return <p key={elem.name}> {elem.name}</p>;
+          return (
+            <>
+              <div className="container">
+                <p className="category" key={elem.name}>
+                  {elem.name}
+                </p>
+
+                <div className="dish-detail-all">
+                  {elem.meals.map((elem2, index) => {
+                    return (
+                      <>
+                        <div className="dish-detail">
+                          <div className="text">
+                            <p className="dish-title" key={elem2.title}>
+                              {elem2.title}
+                            </p>
+                            <p className="dish-description" key={elem2.index}>
+                              {elem2.description}
+                            </p>
+                            <p className="dish-price" key={elem2.index}>
+                              {elem2.price} €
+                            </p>
+                            <p key={elem2.index}> {elem2.popular}</p>
+                          </div>
+
+                          <img src={elem2.picture} alt="dish-image" />
+                        </div>
+                      </>
+                    );
+                  })}
+                </div>
+              </div>
+            </>
+          );
         })}
       </section>
     </>
